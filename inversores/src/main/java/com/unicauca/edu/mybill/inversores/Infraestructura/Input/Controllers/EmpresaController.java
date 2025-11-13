d06ccf7f2c8fde006b0e7840ff843501866dec19
Jeison Andrés Vallejo Gómez <jeisonvallejo@unicauca.edu.co>
2025-11-13 18:38:24 -0500

    Empresa Controller Conection
    Añadir el @CrossOrigin para integrar con el front

--- inversores/src/main/java/com/unicauca/edu/mybill/inversores/Infraestructura/Input/Controllers/EmpresaController.java
+++ inversores/src/main/java/com/unicauca/edu/mybill/inversores/Infraestructura/Input/Controllers/EmpresaController.java
@@ -15,11 +15,13 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/empresas")
 public class EmpresaController {
 
